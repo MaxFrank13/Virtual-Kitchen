bb923d2caa12b7bb541ef51cf0ce7c603ba6f463
Max <maxfrank13@gmail.com>
2022-01-11 00:57:55 -0500

    changed health bars

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ let editElement;
 let editFlag = false;
 let editID = "";
 
-// const images = document.querySelector(".section-img");
-// console.log(images);
-// const closer = document.querySelector(".close");
-// console.log(closer);
-
 window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     const navbar = document.querySelector(".links-container");
@@ -26,18 +21,20 @@ window.addEventListener("scroll", function () {
     const scrollHeight = window.pageYOffset;
     const navHeight = header.getBoundingClientRect().height;
     if (scrollHeight > navHeight) {
-      navbar.classList.add("fixed-nav");
+        navbar.classList.add("fixed-nav");
     } else {
-      navbar.classList.remove("fixed-nav");
+        navbar.classList.remove("fixed-nav");
     }
     // setup back to top link
-  
+
     if (scrollHeight > 400) {
-      topLink.classList.add("show-link");
+        topLink.classList.add("show-link");
     } else {
-      topLink.classList.remove("show-link");
+        topLink.classList.remove("show-link");
     }
-  });
+});
+
+window.addEventListener("DOMContentLoaded", setHealthBars);
 
 // | Event Listeners |
 
@@ -67,7 +64,11 @@ function addItem(e) {
     if (!lctn) {
         displayAlert("please indicate where to put item", "danger");
     }
-    if (value !== "" && !editFlag) {
+    console.log(typeof value);
+    console.log(checkLocalStorage(value));
+    if (checkLocalStorage(value)) {
+        displayAlert("item taken, rewrite name or delete old item from kitchen");
+    } else if (value !== "" && !editFlag) {
         const element = document.createElement("article");
         let attr1 = document.createAttribute("data-id");
         attr1.value = id;
@@ -92,35 +93,36 @@ function addItem(e) {
         <div class="health-bar" data-expire="${attr2.value}" id="${value}"></div>
       `;
 
-      // add event listeners to buttons made in template string
-      const deleteBtn = element.querySelector(".delete-btn");
-      deleteBtn.addEventListener("click", deleteItem);
-      const editBtn = element.querySelector(".edit-btn");
-      editBtn.addEventListener("click", editItem);
-      
-      let lctn_div = document.getElementById(lctn);
+        // add event listeners to buttons made in template string
+        const deleteBtn = element.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", deleteItem);
+        const editBtn = element.querySelector(".edit-btn");
+        editBtn.addEventListener("click", editItem);
 
-      lctn_div.appendChild(element);
+        let lctn_div = document.getElementById(lctn);
 
-      displayAlert("item added to the list", "success");
+        lctn_div.appendChild(element);
 
-      container.classList.add("show-container");
+        displayAlert("item added to the list", "success");
 
-      addToLocalStorage(id, value, expire, lctn);
-      setBackToDefault();
-      setHealthBars();
+        container.classList.add("show-container");
+
+        addToLocalStorage(id, value, expire, lctn);
+        setBackToDefault();
+        setHealthBars();
+
 
     } else if (value !== "" && editFlag) {
         editElement.innerHTML = value;
         displayAlert("value changed", "success");
-    
+
         // edit  local storage
         editLocalStorage(editID, value);
         setBackToDefault();
-            
-      } else {
+
+    } else {
         displayAlert("please enter value", "danger");
-      }
+    }
 }
 
 function displayAlert(text, action) {
@@ -136,9 +138,9 @@ function displayAlert(text, action) {
 function clearItems() {
 
     var elements = document.getElementsByClassName("grocery-item");
-    while(elements.length > 0){
-            elements[0].parentNode.removeChild(elements[0]);
-        }
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
 
     container.classList.remove("show-container");
     displayAlert("empty list", "danger");
@@ -148,13 +150,12 @@ function clearItems() {
 
 function deleteItem(e) {
     const element = e.currentTarget.parentElement.parentElement;
-    console.log(element);
+
     const id = element.dataset.id;
     const lctn = element.classList[1];
 
     let parent_div = document.getElementById(lctn);
-    console.log(lctn);
-    console.log(parent_div);
+
     parent_div.removeChild(element);
 
     if (list.children.length === 0) {
@@ -189,35 +190,48 @@ function setBackToDefault() {
 function setHealthBars() {
     let items = getLocalStorage();
     let currentDay = new Date().getTime();
-    items = items.map(function(item) {
-        const exprTotal = item.expire - item.id;
-        const exprRemaining = item.expire - currentDay;
-
-        const percentRemaining = exprRemaining / exprTotal * 100;
-
+    items = items.map(function (item) {
+        const expire = item.expire - item.id;
+        const oneDay = 24 * 60 * 60 * 1000;
+        let days = Math.floor(expire / oneDay);
+        
         let healthBar = document.getElementById(item.value);
-        healthBar.style.width = `${percentRemaining}%`;
-
-        if (percentRemaining > 70) {
-            healthBar.style.background = "green";
-        } else if (percentRemaining > 25) {
+        healthBar.textContent = `${days} days`
+        if (days <= 1) {
+            healthBar.style.background = "red";
+        } else if (days < 10) {
             healthBar.style.background = "yellow";
         } else {
-            healthBar.style.background = "red";
+            healthBar.style.background = "green";
         }
+        // const exprTotal = item.expire - item.id;
+        // const exprRemaining = item.expire - currentDay;
+        // const percentRemaining = exprRemaining / exprTotal * 100;
+        // if (percentRemaining > 70) {
+        //     healthBar.style.background = "green";
+        //     healthBar.textContent = `${exprRemaining}`
+        // } else if (percentRemaining > 25) {
+        //     healthBar.style.background = "yellow";
+        // } else {
+        //     healthBar.style.background = "red";
+        // }
     })
+}
+
+function getDaysLeft() {
+
 }
 
 function expandList(e) {
     let target_div = e.currentTarget.nextElementSibling;
-    console.log(target_div);
+
     target_div.classList.add("show-section");
     e.currentTarget.classList.add("remove-img");
 };
 
 function minimizeItems(e) {
     let target = e.currentTarget.parentElement;
-    console.log(target.previousElementSibling);
+
     target.classList.remove("show-section");
     target.previousElementSibling.classList.remove("remove-img");
 };
@@ -233,9 +247,9 @@ function addToLocalStorage(id, value, expire, lctn) {
 
 function getLocalStorage() {
     if (localStorage.getItem("list")) {
-       return JSON.parse(localStorage.getItem("list"))
+        return JSON.parse(localStorage.getItem("list"))
     } else {
-       return [];
+        return [];
     }
 }
 
@@ -254,7 +268,7 @@ function removeFromLocalStorage(id) {
 function editLocalStorage(id, value) {
     let items = getLocalStorage();
 
-    items = items.map(function(item) {
+    items = items.map(function (item) {
         if (item.id === id) {
             item.value = value;
         }
@@ -263,13 +277,28 @@ function editLocalStorage(id, value) {
     localStorage.setItem("list", JSON.stringify(items));
 }
 
+function checkLocalStorage(grocery) {
+    let items = getLocalStorage();
+    console.log(items);
+    let nameTaken;
+    items.forEach(function (item) {
+        console.log(item.value);
+        if (grocery === item.value) {
+            nameTaken = true;
+        } else {
+            nameTaken = false;
+        }
+    })
+    return nameTaken;
+}
+
 // | Setup Items |
 
 function setupItems() {
     let items = getLocalStorage();
 
     if (items.length > 0) {
-        items.forEach(function(item) {
+        items.forEach(function (item) {
             createListItem(item.id, item.value, item.expire, item.lctn);
         });
         container.classList.add("show-container");
@@ -305,7 +334,7 @@ function createListItem(id, value, expire, lctn) {
     deleteBtn.addEventListener("click", deleteItem);
     const editBtn = element.querySelector(".edit-btn");
     editBtn.addEventListener("click", editItem);
-  
+
     // append child
     let lctn_div = document.getElementById(lctn);
 
@@ -326,26 +355,26 @@ var recipes = {
         "red sauce",
         "basil",
         "lasagna noodles"
-    ], 
+    ],
     name: "pesto trenette",
     ingredients: [
         "pesto",
         "cheese",
         "noodles"
-    ], 
+    ],
     name: "personal pizzas",
     ingredients: [
         "dough",
         "cheese",
         "red sauce"
-    ], 
+    ],
     name: "burgers",
     ingredients: [
         "buns",
         "burgers",
         "tomato",
         "pickles"
-    ], 
+    ],
     name: "falafel",
     ingredients: [
         "chickpeas",
